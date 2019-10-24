2ded0ca78a6e5e85cfa25709d42cd0bd5a0e3a16
Mark Schneemann <git@f5w.de>
2019-10-24 12:56:15 +0200

    selenium for alert verification

--- scan.py
+++ scan.py
@@ -7,84 +7,59 @@ import random
 import xpath_soup
 from lxml import etree
 import lxml.html
+from selenium import webdriver
 from sys import stdout
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-url = "https://xss-game.appspot.com/level5/frame/signup?next="
-
-username = ""
-password = ""
+url = "https://xss-game.appspot.com/level1/frame?query="
 
 results = set()
 
-def findInputInCode():
-	input = "wherecanyoufindthisstring"
-	sample = requests.get(url + input, auth=(username, password))
-	search_in = ["img", "a", "script", "div", "span"]
-	xpathes = []
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-	soup = BeautifulSoup(sample.text, "lxml")
-	elems = soup.find_all(string=re.compile(input))
-	for elem in elems:
-		xpathes.append(xpath_soup.xpath_soup(elem))
-
-	found_in = soup.find_all("")
-	for search in search_in:
-		found_in += soup.find_all(search)
-
-	for elem in found_in:
-		if input in str(elem):
-			xpathes.append(xpath_soup.xpath_soup(elem))
-	return {x for x in xpathes}
-
-def inject(injection, xpathes, original):
-	r = requests.get(url + injection, auth=(username, password), stream=True)
-	if r.status_code is 200:
-		verify = verifyResultsStupid(r, xpathes, original)
-		if verify != "ok":
-			if verify == "plain input worked":
-				results.add("injection of: " + injection)
-				stdout.write("+")
-			else:
-				results.add(verify)
-				stdout.write("~")
-		else:
-			stdout.write("-")
-		stdout.flush()
+def inject(injection, browser):
+	browser.get(url + injection)
+	try:
+		WebDriverWait(browser, 3).until(EC.alert_is_present(), 'Timed out waiting for alerts to appear')
+		alert = browser.switch_to.alert
+		alert.accept()
+		print(bcolors.FAIL + "XSS" + " for " + injection + bcolors.ENDC)
+		results.add(injection)
+	except TimeoutException:
+		print(bcolors.OKGREEN + "OK" + bcolors.ENDC + " for "+ injection)
 
 def scan():
+	browser = webdriver.Firefox()
+	browser.get(url)
 	print("Scanning: " + url)
-	xpathes = findInputInCode();
-	print("=" * 60)
-	print("XPathes to the injected code are:")
-	for xpath in xpathes:
-		print("\t" + xpath)
-
 	print("=" * 60)
 	check_headers()
 
 	file = open("xss.txt","r")
 	print("=" * 60)
-	print("Testing predefined patterns: xss.txt + mutations")
+	print("Testing predefined patterns: xss.txt + mutations\n")
 	for injection in file:
 		if injection != "":
 			injection_transformations = generate_transformations(injection[:-1])
 			for xss in injection_transformations:
-				inject(xss, xpathes, injection_transformations[0])
+				inject(xss, browser)
+	browser.quit()
 	print("")
 
 	print("=" * 60)
-	print("Have a closer look at:")
+	print("Possible XSS Vulnerabilities:")
 	for result in results:
 		print("\t" + result)
-
-def verifyResultsStupid(r, xpath, xss):
-	if xss.lower() in r.text.lower():
-		return "plain input worked"
-
-	if "&#" in r.text.lower():
-			return "unicode hex might work (found &#...)"
-
-	return "ok"
 
 def charToHex(c):
 	return "\\x" + format(ord(c), "x")
@@ -126,7 +101,6 @@ def generate_transformations(injection):
 	injection_transformations = [injection,
                 encode_url(injection),
                 encode_url(encode_url(injection)),
-		encode_url(encode_url(encode_url(injection))),
 		randomizeUpperLower(injection),
 		allUpper(injection)]
 	return injection_transformations
@@ -138,14 +112,14 @@ def check_headers():
     	'User-Agent': useragentheader,
     	'From': fromheader
 		}
-	r = requests.get(url + "abc", auth=(username, password))
+	r = requests.get(url + "abc")
 	if useragentheader.lower() in r.text.lower():
-		print("User-Agent found in html")
+		print(bcolors.WARNING + "Warning" + bcolors.ENDC + "\tUser-Agent found in html")
 	else:
-		print("User-Agent not found in html")
+		print(bcolors.OKGREEN + "OK" + bcolors.ENDC + "\tUser-Agent not found in html")
 	if fromheader.lower() in r.text.lower():
-		print("From-Header found in html")
+		print(bcolors.WARNING + "Warning" + bcolors.ENDC + "\tFrom-Header found in html.")
 	else:
-		print("From-Header mot found in html")
+		print(bcolors.OKGREEN + "OK" + bcolors.ENDC + "\tFrom-Header mot found in html")
 
 scan()
